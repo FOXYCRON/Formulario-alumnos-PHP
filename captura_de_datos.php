45f3d233833cc5cbf10a16a56904dfc890d4ef95
FOXYCRON <57571091+FOXYCRON@users.noreply.github.com>
2022-06-24 12:21:09 -0600

    Add files via upload

---
+++ captura_de_datos.php
@@ -0,0 +1,29 @@
+<html>
+   <head>
+       <title> Captura De Datos </title>
+   </head>
+   <body>
+  <?php
+
+
+ echo "Nombre del alumno(a): ";
+ echo $_REQUEST['nombre'];
+  echo "<br>";
+  echo "<br>";
+ echo "Apllido del alumno(a): ";
+ echo $_REQUEST['apellido'];
+  echo "<br>";
+  echo "<br>";
+ echo "Grado del alumno(a): ";
+ echo $_REQUEST['grado'];
+  echo "<br>";
+  echo "<br>";
+ echo "Grupo del alumno(a): ";
+ echo $_REQUEST['grupo'];
+  echo "<br>";
+  echo "<br>";
+
+ ?>
+
+ </body>
+</html>
---
+++ entrada_de_datos.php
@@ -0,0 +1,30 @@
+<html>
+   <head>
+      <title> Entrada de datos </title>
+   </head>
+   <body>
+   
+   <form method = "post" action = "captura_de_datos.php">
+   <p> Captura De Datos <p>
+   <br>
+      Ingrese Su Nombre:
+   <input type = "text" name = "nombre">
+   <br>
+   <br>
+      Ingrese Su Apellido:
+   <input type = "text" name = "apellido">
+   <br>
+   <br>
+      Ingrese Su Grado:
+   <input type = "text" name = "grado">
+   <br>
+   <br>
+      Ingrese Su Grupo:
+   <input type = "text" name = "grupo">
+   <br>
+   <br>
+   <input type = "submit" value = "confirmar">
+   </from>
+
+ </body>
+</html>
